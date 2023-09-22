bde87cadcf3c20573606fbd7a501e1fa543fda9d
cinta <cintatafur@gmail.com>
2023-09-22 11:12:59 +0200

    🚀 Init proyect(creo rutas):en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+/* Mis componentes */
+import { ErrorComponent } from './error/error.component';
+import { HelloComponent } from './hello/hello.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'hello/:name', component: HelloComponent },
+  { path: '**', component: ErrorComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ErrorComponent, HelloComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
