dfd7326a07ff9166b620f3eaf204f5090e1a843b
cinta <cintatafur@gmail.com>
2023-09-25 12:29:53 +0200

    🎉 Rutas(Routes, routerLink, routerLinkActive, Router)

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.scss']
+  styleUrls: ['./hello.component.scss'],
 })
-export class HelloComponent {
+export class HelloComponent implements OnInit {
+  name!: string;
 
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.name = this.route.snapshot.params['name'];
+  }
 }
