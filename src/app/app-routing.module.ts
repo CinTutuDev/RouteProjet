cfd96a4e7e651758588214f03ce8bba1a911403f
cinta <cintatafur@gmail.com>
2023-09-25 16:44:47 +0200

    📌 Practica propuesta Módulo5 punto 11(redirectTo)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
-import { ErrorComponent } from './error/error.component';
-import { HelloComponent } from './hello/hello.component';
-import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ErrorComponent } from './error/error.component';
+import { HelloComponent } from './hello/hello.component';
+import { HomeComponent } from './home/home.component';
+
 const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'hello/:name', component: HelloComponent },
   { path: '**', component: ErrorComponent },
 ];
